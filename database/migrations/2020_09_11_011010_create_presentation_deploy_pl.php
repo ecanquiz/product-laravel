34ccb623daf896ec9006cd68832e61df416f884a
Ernesto Canquiz <caribestic123@gmail.com>
2025-02-25 16:17:10 -0400

    fix: When N/A or in Bulk it does not show the packaging well

--- database/migrations/2020_09_11_011010_create_presentation_deploy_pl.php
+++ database/migrations/2020_09_11_011010_create_presentation_deploy_pl.php
@@ -19,32 +19,35 @@ CREATE OR REPLACE FUNCTION public.presentation_deploy(i_id integer)
   RETURNS character AS
 \$BODY\$
 DECLARE
-        o_return character varying;
-        v_arr json[];
-        v_uni_med character varying;
-        v_arr_union character varying[];
-        v_n integer;
+    o_return character varying;
+    v_arr json[];
+    v_uni_med character varying;
+    v_arr_union character varying[];
+    v_n integer;
 BEGIN
-         o_return := '';
-         SELECT (ARRAY(select json_array_elements(A.packing::json))), B.measure_unit  INTO v_arr, v_uni_med         
-         FROM public.presentations A 
-         INNER JOIN public.products B ON A.product_id = B.id
-         WHERE A.id=i_id;         
-         v_arr_union = ARRAY[' DE ', ' CON ']; 
-         v_n = 1;        
-         FOR i IN array_lower(v_arr, 1) .. array_upper(v_arr, 1) LOOP                  
-                  IF  i = 1 THEN
-                      o_return := v_arr[i]->'packing' || v_arr_union[v_n] || (v_arr[i]->'quantity'::text) || ' ' || v_uni_med;
-                  ELSE
-                      o_return := (v_arr[i]->'packing') || v_arr_union[v_n] || (v_arr[i]->'quantity') || ' ' || o_return;
-                  END IF;
-                  v_n = v_n + 1;
-                  IF v_n = 3 THEN
-                      v_n = 1;
-                  END IF;
-         END LOOP;
-         
-        RETURN (replace(o_return, '\"', ''));
+    o_return := '';
+    SELECT (ARRAY(select json_array_elements(A.packing::json))), B.measure_unit  INTO v_arr, v_uni_med         
+    FROM public.presentations A 
+    INNER JOIN public.products B ON A.product_id = B.id
+    WHERE A.id=i_id;         
+    v_arr_union = ARRAY[' DE ', ' CON ']; 
+    v_n = 1;        
+    FOR i IN array_lower(v_arr, 1) .. array_upper(v_arr, 1) LOOP
+        IF (v_arr[i]->'packing')::text = '\"\"' THEN
+            o_return := (v_arr[i]->'quantity'::text) || ' ' || v_uni_med;
+        ELSE
+            IF  i = 1 THEN
+                o_return := v_arr[i]->'packing' || v_arr_union[v_n] || (v_arr[i]->'quantity'::text) || ' ' || v_uni_med;
+            ELSE
+                o_return := (v_arr[i]->'packing') || v_arr_union[v_n] || (v_arr[i]->'quantity'::text) || ' ' || o_return;
+            END IF;
+            v_n = v_n + 1;
+            IF v_n = 3 THEN
+                v_n = 1;
+            END IF;				  
+        END IF;
+    END LOOP;
+    RETURN (replace(o_return, '\"', ''));
 END;
 \$BODY\$
   LANGUAGE plpgsql VOLATILE
